b00b5378baf9d68b1cbac13984701c750eba648b
Maykon <maykonbw@gmail.com>
2023-03-02 15:53:55 -0300

    commit inicial

--- main.js
+++ main.js
@@ -1,26 +1,18 @@
 $(document).ready(function() {
-    $('header button').click(function() { 
-        $('form').slideDown();
-    })
-
-    $('#botao-cancelar').click(function(){ 
-        $('form').slideUp();
-    })
 
     $('form').on('submit', function(e) {
         e.preventDefault();
-        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
-        const novoItem = $('<li style="display:none"> </li>');
-        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
-        $(`
-            <div class="overlay-imagem-link"> 
-                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real"> 
-                    Ver imagem em tamanho real
-                </a>
-            <div/>
-            `).appendTo(novoItem);
-            $(novoItem).appendTo('ul');
-            $(novoItem).fadeIn(1000);
-            $('#endereco-imagem-nova').val('')
+        const enderecoDaNovaTarefa = $('#endereco-nova-tarefa').val();
+        const novoItem = $('<li> </li>');
+        $(` <p> </p>`).appendTo(novoItem);
+        $(`<p> ${enderecoDaNovaTarefa}</p>`).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $('#endereco-nova-tarefa').val('')
+        
+        $('ul').on('click','li',function(){
+        $('li').toggleClass('riscado');
+            
+        })
+        
     })
-})
+    })
